f87c363f1983fd29d8fa9e24b6aa18cbd56965c5
pr0f3ss <filipdobro1999@gmail.com>
2021-03-21 15:09:15 +0100

    added support for command line arguments

--- nss_benchmark.c
+++ nss_benchmark.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h> 
 #include <time.h>
 #include <openssl/bn.h>
 #include "nss_pga.h"
 
-int main(){
+int main(int argc, char **argv){
 
 	BIGNUM *p;
 	p = BN_new();
@@ -11,54 +12,45 @@ int main(){
 	// seen as set
 	int k = 1024; //bitsize
 	int u = 10; // max nss_iter rounds
-	int t = 5;
+	int t = 5; // MR rounds
 
-	int r = 1024;
-	int l = 4000; 
+	int l = 4000; // max deviation
 
-	int r_inc = 16;
+	int r = argv[1]; // get r from program argument
+
 	int l_inc = 512;
 
+	char filename[64];
+	sprintf(filename, "data/nss_benchmark/nss_analysis_r%d.csv", r);
 	FILE *fd;
-	fd = fopen("nss_analysis.csv", "w+");
+	fd = fopen(filename, "w+");
 	fprintf(fd,"r, l, avgruntime\n");
 
-	while(r<8192){
+	while(l<0x10000){
+		
+		clock_t start, end;
+		double cpu_time_used;
 
-		l = 4000;
-		l_inc = 512;
+		start = clock();
 
-		while(l<0x20000){
-			
-			clock_t start, end;
-			double cpu_time_used;
-
-			start = clock();
-
-			for(int i=0; i<8192; i++){
-				//int nss_pga(BIGNUM *p, int k, int t, int u, int r, int l);
-				int returncode = nss_pga(p, k, t, u, r, l);
-			}
-
-			end = clock();
-    		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-			printf("Test done for: r=%d, l=%d in %fs\n", r, l, cpu_time_used);
-			fprintf(fd, "%d, %d, %f\n", r, l, cpu_time_used);
-
-			if(l>0x4000){
-				l_inc = 1024;
-			}
-
-			l += l_inc;
+		for(int i=0; i<8192; i++){
+			//int nss_pga(BIGNUM *p, int k, int t, int u, int r, int l);
+			int returncode = nss_pga(p, k, t, u, r, l);
 		}
 
-		if(r>3000){
-			r_inc = 64;
+		end = clock();
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		cpu_time_used /= 8192.;
+
+		fprintf(stdout, "Test done for: r=%d, l=%d in %fs\n", r, l, cpu_time_used);
+		fprintf(fd, "%d, %d, %f\n", r, l, cpu_time_used);
+
+		if(l>0x4000){
+			l_inc = 1024;
 		}
 
-		r += r_inc;
-	}
+		l += l_inc;
+	}	
 	
 	BN_free(p);
 	fclose(fd);
