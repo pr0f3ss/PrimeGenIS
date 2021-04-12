c5d8486ce567d9e83f28c6b20a8c30d8fce36ef9
pr0f3ss <filipdobro1999@gmail.com>
2021-04-12 11:01:14 +0200

    New plots

--- generate_benchmark_plot.py
+++ generate_benchmark_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
 
-csv_data = genfromtxt('data/dirichlet_benchmark/dirichlet_nss_analysis_r33.csv', delimiter=',') #r, l, runtime for 8192 primes
+csv_data = genfromtxt('data/nss_benchmark/dirichlet_sieve/2ndrun_4k_120k_r33.csv', delimiter=',') #r, l, runtime for 8192 primes
 
 l = csv_data[1:,1]
 time = csv_data[1:,2]
@@ -16,5 +16,5 @@ plt.title('Average runtime for k=1024, r=33');
 plt.plot(l, time)
 
 
-plt.savefig("plots/dirichlet_nss_analysis_r33.png")
+plt.savefig("plots/nss_dir_2ndrun.png")
 
