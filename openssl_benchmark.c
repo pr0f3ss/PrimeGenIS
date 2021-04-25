1fd2bbb04cbfbe21fd406c78a56715ec217fce1d
pr0f3ss <filipdobro1999@gmail.com>
2021-04-25 20:58:35 +0200

    add changed params

--- openssl_benchmark.c
+++ openssl_benchmark.c
@@ -12,28 +12,37 @@ int main(int argc, char **argv){
 
 	// seen as set
 	int k = 1024; //bitsize
-	int t = 5; // MR rounds
-	int l = 4000; // max deviation
+	int t;
+	int l = 0; // max deviation
 
 	int r = atoi(argv[1]); // get r from program argument
 
 	int l_inc = 512;
 
 	char filename[256];
-	sprintf(filename, "data/openssl_benchmark/nss_sieve/1strun_r%d.csv", r);
+	sprintf(filename, "data/openssl_benchmark/openssl_sieve/2ndrun_r%d.csv", r);
 	FILE *fd;
 	fd = fopen(filename, "w+");
 	fprintf(fd,"r, l, avgruntime\n");
 
 	while(l<1){
-		
+		FILE *fd_params = fopen("data/optimal_params/openssl_pga/openssl_k1024_r16_r8080.csv");
+		int curr_r = 0;
+		int curr_t;
+
+		while(curr_r != r){
+			fscanf(fd_params, "%d,%d", &curr_r, &curr_t);
+		}
+
+		t = curr_t;
+
 		clock_t start, end;
 		double cpu_time_used;
 
 		start = clock();
 
 		for(int i=0; i<8192; i++){
-			int returncode = openssl_pga(p, k, t, r, l, nss_generate_sieve, nss_sieve);
+			int returncode = openssl_pga(p, k, t, r, l, openssl_generate_sieve, openssl_sieve);
 		}
 
 		end = clock();
