4f92be9b2a8b7e0f9dbc3ff9a75abd9fddd6d913
pr0f3ss <filipdobro1999@gmail.com>
2021-04-25 17:24:50 +0200

    added files

--- generate_optimal_pga_params.py
+++ generate_optimal_pga_params.py
@@ -2,6 +2,7 @@ from pga_params import nss_params
 from pga_params import ossl_params
 from pga_params import nat_params
 import csv
+import sys
 
 if __name__ == "__main__":
 	k=1024
@@ -10,26 +11,19 @@ if __name__ == "__main__":
 	r_bound = 3024
 	l_bound = 12000
 
-	# #nss param generator
-	# filepath = "data/optimal_params/nss_pga/"
-	# filename = filepath+"nss_k1024_r16_r8080_l700_l65536.csv"
-	# r=16
-	# l=700
-	# with open(filename, 'w', newline='') as file:
-	#     writer = csv.writer(file)
-	#     writer.writerow(["r", "l", "U", "t"])
-	#     while(r <= r_max):
-	#     	l = 700
-	#     	r_inc = 64 if r >= r_bound else 16
-	#     	while(l <= l_max):
-	#     		l_inc = 1024 if l > l_bound else 512
-	#     		U, t = nss_params(k, l, r)
-	#     		writer.writerow([r, l, U, t])
-	#     		l += l_inc
-	#     	r += r_inc
-	#     	print(r)
-
-	# print("finished nss param generation")
+	#nss param generator
+	filepath = "data/optimal_params/nss_pga/"
+	r=sys.argv[1]
+	filename = filepath+"nss_k1024_r"+r+"_l700_l65536.csv"
+	with open(filename, 'w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow(["r", "l", "U", "t"])
+		l = 700
+		while(l <= l_max):
+			l_inc = 1024 if l > l_bound else 512
+			U, t = nss_params(k, l, r)
+			writer.writerow([r, l, U, t])
+			l += l_inc
 
 	# #ossl param generator
 	# filepath = "data/optimal_params/openssl_pga/"
@@ -47,17 +41,17 @@ if __name__ == "__main__":
 	# print("finished openssl param generation")
 
 	#nat param generator
-	filepath = "data/optimal_params/nat_pga/"
-	filename = filepath+"nat_k1024_r16_r8080csv"
-	r=16
-	l=64000
-	with open(filename, 'w', newline='') as file:
-	    writer = csv.writer(file)
-	    writer.writerow(["r", "t"])
-	    while(r <= r_max):
-	    	r_inc = 64 if r >= r_bound else 16
-    		t = nat_params(k, l, r)
-    		writer.writerow([r, t])
-	    	r += r_inc
-	print("finished nat param generation")
+	# filepath = "data/optimal_params/nat_pga/"
+	# filename = filepath+"nat_k1024_r16_r8080csv"
+	# r=16
+	# l=64000
+	# with open(filename, 'w', newline='') as file:
+	#     writer = csv.writer(file)
+	#     writer.writerow(["r", "t"])
+	#     while(r <= r_max):
+	#     	r_inc = 64 if r >= r_bound else 16
+    # 		t = nat_params(k, l, r)
+    # 		writer.writerow([r, t])
+	#     	r += r_inc
+	# print("finished nat param generation")
 
