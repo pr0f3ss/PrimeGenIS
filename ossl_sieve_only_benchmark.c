42e39077b09e0cafb7642fddb3031887e3e5a6d3
pr0f3ss <dofilip@student.ethz.ch>
2021-05-04 01:25:06 +0200

    added primitive ossl implementation benchmark for debugging

--- ossl_sieve_only_benchmark.c
+++ ossl_sieve_only_benchmark.c
@@ -13,9 +13,14 @@ int main(int argc, char **argv){
     BIGNUM *n0;
 	n0 = BN_new();
 
+	// create buffer for internal computations
+	BN_CTX *ctx;
+	ctx = BN_CTX_new();
+
+
 	// seen as set
 	int k = 1024; //bitsize
-	int t;
+	int t = 8;
 	int l = 0; // max deviation
 
 	int r = atoi(argv[1]); // get r from program argument
@@ -33,16 +38,23 @@ int main(int argc, char **argv){
     start = clock();
 
     for(int i=0; i<8192; i++){
-        if(!BN_rand(n0, k, BN_RAND_TOP_TWO, BN_RAND_BOTTOM_ODD)){
-            goto free_bn;
-        }
-        unsigned char *sieve;
-        int sieve_sz = 0;
-        if(!openssl_generate_sieve(&sieve, sieve_sz, n0, r)){
-            goto free_bn;
-        }
-        unsigned long it = 0; // is passed on as an iterator variable inside openssl_sieve to do the sieve checking. 
-        ret = openssl_pga(sieve, sieve_sz, p, n0, r, &it, k);
+    	//int iterations = 0;
+    	do{
+	        if(!BN_rand(n0, k, BN_RAND_TOP_TWO, BN_RAND_BOTTOM_ODD)){
+	            goto free_bn;
+	        }
+	        unsigned char *sieve;
+	        int sieve_sz = 0;
+	        if(!openssl_generate_sieve(&sieve, sieve_sz, n0, r)){
+	            goto free_bn;
+	        }
+	        unsigned long it = 0; // is passed on as an iterator variable inside openssl_sieve to do the sieve checking. 
+	        int ret = openssl_sieve(sieve, sieve_sz, p, n0, r, &it, k);
+	        //iterations++;
+
+        }while(!BN_is_prime_fasttest_ex(p, t, ctx, 0, NULL));
+        //printf("iterations: %d, bitsize: %d\n", iterations, BN_num_bits(p));
+
     }
 
     end = clock();
@@ -50,11 +62,13 @@ int main(int argc, char **argv){
     cpu_time_used /= 8192.;
 
     fprintf(fd, "%d, %d, %f\n", r, l, cpu_time_used);
+    printf("Done\n");
 
     free_bn:
         BN_free(p);
         BN_free(n0);
         fclose(fd);
+        BN_CTX_free(ctx);
 
 	return 0;
 }
